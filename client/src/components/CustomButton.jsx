eb1c5ec327bc1afb1fe3084c8c21c427805b7204
stublic.jurica <stublic.jurica@gmail.com>
2023-05-25 13:50:29 +0200

    400 error

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSnapshot } from "valtio";
 import state from '../store';
+import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({type, title, customStyles, handleClick}) => {
 
@@ -10,8 +11,14 @@ const generateStyle = (type) => {
     if (type === 'filled') {
         return{
             backgroundColor: snap.color,
-            color: '#fff'
+            color: getContrastingColor(snap.color)
         }
+    }else if(type === 'outline'){
+      return{
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color
+      }
     }
 }
 
--- client/src/pages/Customiser.jsx
+++ client/src/pages/Customiser.jsx
@@ -12,7 +12,114 @@ import {fadeAnimation, slideAnimation } from '../config/motion'
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customiser = () => {
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  //show tab content depending on the active tab
+
+    const generateTabContent = () => {
+      switch (activeEditorTab) {
+        case "colorpicker":
+          return <ColorPicker/>
+        case "filepicker":
+          return <FilePicker
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
+          />
+        case "aipicker":
+          return <AIPicker
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+          />
+          default:
+            return null;
+      }
+    }
+
+    const handleSubmit = async (type) => {
+      if(!prompt) return alert("Please enter a prompt");
+
+      try {
+        //call backend to generate an ai image
+        setGeneratingImg(true);
+
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          header: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            prompt,
+          })
+        })
+
+        const data = await response.json();
+
+        handleDecals(type, `data:image/png;base64,${data.photo}`)
+      }catch (error){
+        alert(error)
+      }finally{
+        setGeneratingImg(false)
+        setActiveEditorTab("")
+      }
+    }
+
+    const handleDecals = (type, result) => {
+      const decalType = DecalTypes[type];
+
+      state[decalType.stateProperty] = result;
+
+      if (!activeFilterTab[decalType.filterTab]) {
+        handleActiveFilterTab(decalType.filterTab)
+      }
+    }
+
+    const handleActiveFilterTab = (tabName) => {
+      switch (tabName){
+        case "logoShirt":
+          state.isLogoTexture = !activeFilterTab[tabName];
+          break;
+        case "stylishShirt":
+          state.isFullTexture = !activeFilterTab[tabName]
+          default:
+            state.isFullTexture = false;
+            state.isLogoTexture = true;
+      }
+
+      //after setting the state, activeFilterTab is updated
+
+      setActiveFilterTab((prevState) => {
+        return{
+          ...prevState,
+        [tabName]: !prevState[tabName]
+        }
+        
+      })
+    }
+
+const readFile = (type) => {
+  reader(file)
+  .then((result) => {
+    handleDecals(type, result);
+    setActiveEditorTab("")
+  })
+}
+
   return (
     <AnimatePresence>
       {!snap.intro &&(
@@ -29,9 +136,11 @@ const Customiser = () => {
                   <Tab
                   key={tab.name}
                   tab={tab}
-                  handleClick={() => {}}
+                  handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
         </motion.div>
@@ -52,8 +161,8 @@ const Customiser = () => {
                   key={tab.name}
                   tab={tab}
                   isFilteredTab
-                  isActiveTab=""
-                  handleClick={() => {}}
+                  isActiveTab={activeFilterTab[tab.name]}
+                  handleClick={() => handleActiveFilterTab(tab.name)}
                   />
                 ))}
         </motion.div>
