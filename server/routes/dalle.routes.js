87af7f7a760135baeb2698291069314ee9109a45
stublic.jurica <stublic.jurica@gmail.com>
2023-05-26 11:53:41 +0200

    env removed

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
 const config = new Configuration({
+  organization: "org-InXo2Wj4kqzFWVvq75w4FyDV",
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
 
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
-})
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
 
-router.route('/').post(async (req, res) => {
+router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
-      prompt,
+      prompt: prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
+      size: "1024x1024",
+      response_format: "b64_json",
     });
 
     const image = response.data.data[0].b64_json;
@@ -32,8 +33,10 @@ router.route('/').post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" })
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(image);
+    console.log(photo);
   }
-})
+});
 
 export default router;
